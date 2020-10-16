65faf5e07193bee807bba8e6e6d35bdfd207bfae
mostafa <me@alpha.com>
2020-10-16 21:38:06 +0200

    userauth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import './App.scss'
+import ApolloProvider from './ApolloProvider';
+import { AuthProvider } from './context/auth';
+import { MessageProvider } from './context/message'
+import Auth from './utils/Auth';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider>
+      <AuthProvider>
+        <MessageProvider>
+          <BrowserRouter>
+            <Container className="pt-5">
+              <Switch>
+                <Auth exact path="/" component={Home} authenticated/>
+                <Auth path="/register" component={Register} guest/>
+                <Auth path="/login" component={Login} guest/>
+              </Switch>
+            </Container>
+          </BrowserRouter>
+        </MessageProvider>
+      </AuthProvider>
+    </ApolloProvider>
   );
 }
 
